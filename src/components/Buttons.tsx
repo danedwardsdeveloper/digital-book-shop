ddb6c2cdbc4f926f37e160eafca88d8e659111cb
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-09 15:07:59 +0100

    Update import path

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import clsx from 'clsx';
 
 import { CartItem } from '@/types';
-import { useCart } from '@/hooks/useCart';
+import { useCart } from './Providers';
 
 const baseButtonStyles = clsx(
 	'font-semibold',
