91aa995506df05f75baacbbb3bf60202a2a0b3f3
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-09 15:04:26 +0100

    Change loggedIn to signedIn

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 import { useState } from 'react';
-import { useApiContext } from '../../components/Providers';
+import { useApiContext } from '@/components/Providers';
 
 import { Form, FormLink, FormSpacer, Input } from '@/components/Form';
 import { FeedbackMessage } from '@/components/FeedbackMessage';
 import { SubmitButton } from '@/components/Buttons';
-import { ApiResponse } from '@/types';
+import type { ApiResponse } from '@/types';
 
-export default function CreateAccount() {
-	const { updateApiResponse } = useApiContext();
+export default function SignIn() {
+	const { updateApiResponse, mergeCartsOnLogin } = useApiContext();
 
 	const [email, setEmail] = useState('dan@gmail.com');
 	const [password, setPassword] = useState('securePassword');
@@ -30,18 +30,20 @@ export default function CreateAccount() {
 
 			const data: ApiResponse = await response.json();
 
-			if (response.ok) {
+			if (response.ok && data.user) {
+				await mergeCartsOnLogin(data.user);
+
 				updateApiResponse({
-					message: `Welcome ${data.user?.name}` || 'Sign in successful',
+					message: `Welcome ${data.user.name}` || 'Sign in successful',
 					status: data.status,
-					loggedIn: true,
+					signedIn: true,
 					user: data.user,
 				});
 			} else {
 				updateApiResponse({
 					message: data.message || 'An unexpected error occurred',
 					status: data.status,
-					loggedIn: false,
+					signedIn: false,
 					user: null,
 				});
 			}
@@ -49,7 +51,7 @@ export default function CreateAccount() {
 			updateApiResponse({
 				message: 'An error occurred during sign in',
 				status: 'error',
-				loggedIn: false,
+				signedIn: false,
 				user: null,
 			});
 		} finally {
@@ -78,7 +80,7 @@ export default function CreateAccount() {
 			/>
 			<FeedbackMessage />
 			<SubmitButton cta="Sign in" disabled={isLoading} />
-			<FormLink target={'/sign-in'} text={'Sign in instead'} />
+			<FormLink target="/create-account" text="Create account instead" />
 		</Form>
 	);
 }
