e3220baad88fc856ff9fd289a25dd766976b32ae
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-09 14:57:40 +0100

    Update OrderTable props

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,19 +1,28 @@
+'use client';
 import { NavButton } from '@/components/Buttons';
 import Container from '@/components/Container';
 import OrderTable from '@/components/OrderTable';
-import { books } from '@/library/books';
+import { useApiContext } from '@/components/Providers';
 
 export default function Cart() {
+	const { cart } = useApiContext();
+
+	const activeCartItems = cart.filter((item) => !item.removed);
+
 	return (
 		<div>
-			<OrderTable type={'orderSummary'} books={books} />
+			<OrderTable type={'orderSummary'} />
 			<Container>
 				<NavButton
 					href={'/'}
 					cta={'Continue shopping'}
 					variant={'secondary'}
 				/>
-				<NavButton href={'/checkout'} cta={'Checkout'} />
+				<NavButton
+					href={'/checkout'}
+					cta={'Checkout'}
+					disabled={activeCartItems.length === 0}
+				/>
 			</Container>
 		</div>
 	);
