c1e743391df49c6af2dbd8324e16314a9d89b73c
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-11 12:35:03 +0100

    Comment-out old button components

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,170 +1,170 @@
-'use client';
-import { useState } from 'react';
-import Link from 'next/link';
-import clsx from 'clsx';
+// 'use client';
+// import { useState } from 'react';
+// import Link from 'next/link';
+// import clsx from 'clsx';
 
-import { CartItem } from '@/types';
-import { useCart } from './Providers';
+// import { CartItem } from '@/types';
+// import { useCart } from './Providers';
 
-const baseButtonStyles = clsx(
-	'font-semibold',
-	'w-full p-2',
-	'rounded-md',
-	'transition-colors duration-200'
-);
+// const baseButtonStyles = clsx(
+// 	'font-semibold',
+// 	'w-full p-2',
+// 	'rounded-md',
+// 	'transition-colors duration-200'
+// );
 
-const primaryColourStyles = clsx(
-	'bg-gray-400',
-	'hover:bg-gray-500',
-	'active:bg-gray-600',
-	'text-black'
-);
+// const primaryColourStyles = clsx(
+// 	'bg-gray-400',
+// 	'hover:bg-gray-500',
+// 	'active:bg-gray-600',
+// 	'text-black'
+// );
 
-const secondaryColourStyles = clsx(
-	'bg-gray-200',
-	'border border-gray-300',
-	'hover:bg-gray-300',
-	'active:bg-gray-400',
-	'text-gray-800 hover:text-gray-900'
-);
+// const secondaryColourStyles = clsx(
+// 	'bg-gray-200',
+// 	'border border-gray-300',
+// 	'hover:bg-gray-300',
+// 	'active:bg-gray-400',
+// 	'text-gray-800 hover:text-gray-900'
+// );
 
-const deleteColorStyles = clsx(
-	'bg-red-400',
-	'hover:bg-red-500',
-	'active:bg-red-600',
-	'text-black'
-);
+// const deleteColorStyles = clsx(
+// 	'bg-red-400',
+// 	'hover:bg-red-500',
+// 	'active:bg-red-600',
+// 	'text-black'
+// );
 
-interface SubmitButtonProps
-	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	cta:
-		| 'Sign in'
-		| 'Create account'
-		| 'Buy now'
-		| 'Add to cart'
-		| 'Sign out'
-		| 'In cart'
-		| 'Adding...';
-	variant?: 'primary' | 'secondary';
-	classes?: string;
-}
+// interface SubmitButtonProps
+// 	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+// 	cta:
+// 		| 'Sign in'
+// 		| 'Create account'
+// 		| 'Buy now'
+// 		| 'Add to cart'
+// 		| 'Sign out'
+// 		| 'In cart'
+// 		| 'Adding...';
+// 	variant?: 'primary' | 'secondary';
+// 	classes?: string;
+// }
 
-export function SubmitButton({
-	cta,
-	type = 'submit',
-	variant = 'primary',
-	classes,
-	...props
-}: SubmitButtonProps) {
-	const colourStyles =
-		variant === 'primary' ? primaryColourStyles : secondaryColourStyles;
-	return (
-		<button
-			type={type}
-			className={clsx(baseButtonStyles, colourStyles, classes)}
-			{...props}
-		>
-			{cta}
-		</button>
-	);
-}
+// export function SubmitButton({
+// 	cta,
+// 	type = 'submit',
+// 	variant = 'primary',
+// 	classes,
+// 	...props
+// }: SubmitButtonProps) {
+// 	const colourStyles =
+// 		variant === 'primary' ? primaryColourStyles : secondaryColourStyles;
+// 	return (
+// 		<button
+// 			type={type}
+// 			className={clsx(baseButtonStyles, colourStyles, classes)}
+// 			{...props}
+// 		>
+// 			{cta}
+// 		</button>
+// 	);
+// }
 
-interface DeleteButtonProps
-	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	classes?: string;
-}
+// interface DeleteButtonProps
+// 	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+// 	classes?: string;
+// }
 
-export function DeleteButton({ classes, ...props }: DeleteButtonProps) {
-	return (
-		<button
-			type="button"
-			className={clsx(baseButtonStyles, deleteColorStyles, classes)}
-			{...props}
-		>
-			Delete account
-		</button>
-	);
-}
+// export function DeleteButton({ classes, ...props }: DeleteButtonProps) {
+// 	return (
+// 		<button
+// 			type="button"
+// 			className={clsx(baseButtonStyles, deleteColorStyles, classes)}
+// 			{...props}
+// 		>
+// 			Delete account
+// 		</button>
+// 	);
+// }
 
-interface NavButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	href: string;
-	cta: string;
-	variant?: 'primary' | 'secondary';
-}
+// interface NavButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+// 	href: string;
+// 	cta: string;
+// 	variant?: 'primary' | 'secondary';
+// }
 
-export function NavButton({
-	href,
-	cta,
-	variant = 'primary',
-	...props
-}: NavButtonProps) {
-	const colourStyles =
-		variant === 'primary' ? primaryColourStyles : secondaryColourStyles;
+// export function NavButton({
+// 	href,
+// 	cta,
+// 	variant = 'primary',
+// 	...props
+// }: NavButtonProps) {
+// 	const colourStyles =
+// 		variant === 'primary' ? primaryColourStyles : secondaryColourStyles;
 
-	return (
-		<button
-			className={clsx('text-center', baseButtonStyles, colourStyles)}
-			{...props}
-		>
-			<Link
-				href={href}
-				className="w-full h-full flex items-center justify-center"
-			>
-				{cta}
-			</Link>
-		</button>
-	);
-}
+// 	return (
+// 		<button
+// 			className={clsx('text-center', baseButtonStyles, colourStyles)}
+// 			{...props}
+// 		>
+// 			<Link
+// 				href={href}
+// 				className="w-full h-full flex items-center justify-center"
+// 			>
+// 				{cta}
+// 			</Link>
+// 		</button>
+// 	);
+// }
 
-export function CartButton({ slug }: CartItem) {
-	const { addToCart, isInCart } = useCart();
-	const [isLoading, setIsLoading] = useState(false);
-	const inCart = isInCart(slug);
+// export function CartButton({ slug }: CartItem) {
+// 	const { addToCart, isInCart } = useCart();
+// 	const [isLoading, setIsLoading] = useState(false);
+// 	const inCart = isInCart(slug);
 
-	const handleAddToCart = async () => {
-		if (inCart) return;
+// 	const handleAddToCart = async () => {
+// 		if (inCart) return;
 
-		setIsLoading(true);
-		try {
-			await addToCart(slug);
-		} catch (error) {
-			console.error('Error adding item to cart:', error);
-		} finally {
-			setIsLoading(false);
-		}
-	};
+// 		setIsLoading(true);
+// 		try {
+// 			await addToCart(slug);
+// 		} catch (error) {
+// 			console.error('Error adding item to cart:', error);
+// 		} finally {
+// 			setIsLoading(false);
+// 		}
+// 	};
 
-	return (
-		<SubmitButton
-			classes={'my-8'}
-			cta={inCart ? 'In cart' : isLoading ? 'Adding...' : 'Add to cart'}
-			onClick={handleAddToCart}
-			disabled={inCart || isLoading}
-		/>
-	);
-}
+// 	return (
+// 		<SubmitButton
+// 			classes={'my-8'}
+// 			cta={inCart ? 'In cart' : isLoading ? 'Adding...' : 'Add to cart'}
+// 			onClick={handleAddToCart}
+// 			disabled={inCart || isLoading}
+// 		/>
+// 	);
+// }
 
-interface ToggleCartButtonProps {
-	slug: string;
-	isRemoved: boolean;
-	onToggle: (slug: string) => void;
-}
+// interface ToggleCartButtonProps {
+// 	slug: string;
+// 	isRemoved: boolean;
+// 	onToggle: (slug: string) => void;
+// }
 
-export function ToggleCartButton({
-	slug,
-	isRemoved,
-	onToggle,
-}: ToggleCartButtonProps) {
-	const handleToggle = async () => {
-		onToggle(slug);
-	};
+// export function ToggleCartButton({
+// 	slug,
+// 	isRemoved,
+// 	onToggle,
+// }: ToggleCartButtonProps) {
+// 	const handleToggle = async () => {
+// 		onToggle(slug);
+// 	};
 
-	return (
-		<button
-			onClick={handleToggle}
-			className={'hover:underline text-gray-500'}
-		>
-			{isRemoved ? 'add' : 'remove'}
-		</button>
-	);
-}
+// 	return (
+// 		<button
+// 			onClick={handleToggle}
+// 			className={'hover:underline text-gray-500'}
+// 		>
+// 			{isRemoved ? 'add' : 'remove'}
+// 		</button>
+// 	);
+// }
