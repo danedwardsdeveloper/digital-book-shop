309793b8348df8d6843b6756a1dce18a9bcbcc29
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-14 17:48:37 +0000

    Add env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
+	env: {
+		JWT_SECRET: process.env.JWT_SECRET,
+		STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+		MONGODB_URI: process.env.MONGODB_URI,
+		NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+			process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+	},
 };
 export default nextConfig;
