b92703d83c6736a5fd06f4b03199fa9c3bff3c75
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-14 17:53:59 +0000

    Delete serverEnvironment.ts

--- src/app/api/serverEnvironment.ts
+++
@@ -1,8 +0,0 @@
-import { validateVariable } from '@/library/clientEnvironment';
-
-const jwtSecret = validateVariable('JWT_SECRET');
-const mongoURI = validateVariable('MONGODB_URI');
-const stripeSecretKey = validateVariable('STRIPE_SECRET_KEY');
-const stripeWebhookSecret = validateVariable('STRIPE_WEBHOOK_SECRET');
-
-export { jwtSecret, mongoURI, stripeSecretKey, stripeWebhookSecret };
