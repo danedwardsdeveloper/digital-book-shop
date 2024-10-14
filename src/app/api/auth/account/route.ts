beeddddc73ece6f65d2eb2e1f492d49f813102db
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-14 17:49:50 +0000

    Add route

---
+++ src/app/api/auth/account/route.ts
@@ -0,0 +1,55 @@
+'use server';
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+
+import { jwtSecret } from '@/library/environment';
+import { User, connectToDatabase } from '@/library/User';
+import { Token } from '@/types';
+
+export async function GET() {
+	try {
+		await connectToDatabase();
+
+		const cookieStore = cookies();
+		const token = cookieStore.get('token');
+
+		if (!token) {
+			return NextResponse.json({ error: 'No token found' }, { status: 401 });
+		}
+
+		try {
+			const decodedToken = jwt.verify(token.value, jwtSecret) as Token;
+
+			const user = await User.findById(decodedToken.sub);
+
+			if (!user) {
+				return NextResponse.json(
+					{ error: 'User not found' },
+					{ status: 404 }
+				);
+			}
+
+			return NextResponse.json({
+				status: 'success',
+				message: 'Token is valid',
+				user: {
+					id: user._id,
+					name: user.name,
+					email: user.email,
+					cart: user.cart,
+					purchased: user.purchased,
+				},
+			});
+		} catch (jwtError) {
+			console.error('JWT verification error:', jwtError);
+			return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
+		}
+	} catch (error) {
+		console.error('Account validation error:', error);
+		return NextResponse.json(
+			{ error: 'Internal server error' },
+			{ status: 500 }
+		);
+	}
+}
