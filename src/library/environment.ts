e4eb01b127c52c474a9c24eec9c80a33172ca2af
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-15 11:43:45 +0000

    Simplify environment variable handling

--- src/library/environment.ts
+++ src/library/environment.ts
@@ -1,24 +1,3 @@
-export const jwtSecret = process.env.JWT_SECRET as string;
-if (!jwtSecret) {
-	throw new Error('JWT_SECRET missing');
-}
-
-export const stripeSecretKey = process.env.STRIPE_SECRET_KEY as string;
-if (!stripeSecretKey) {
-	throw new Error('STRIPE_SECRET_KEY missing');
-}
-
-export const mongodbUri = process.env.MONGODB_URI as string;
-if (!mongodbUri) {
-	throw new Error('MONGODB_URI missing');
-}
-
-export const stripePublishableKey = process.env
-	.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string;
-if (!stripePublishableKey) {
-	throw new Error('NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY missing');
-}
-
 export const isProduction = process.env.NODE_ENV === 'production';
 export const isDevelopment = process.env.NODE_ENV === 'development';
 
