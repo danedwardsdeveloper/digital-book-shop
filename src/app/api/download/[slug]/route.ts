52b432f6346bba2b7bcae88137fa9e0758fb9290
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-22 18:14:19 +0100

    Create route.ts

---
+++ src/app/api/download/[slug]/route.ts
@@ -0,0 +1,89 @@
+import { NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+import fs from 'fs/promises';
+import path from 'path';
+
+import { connectToDatabase, User } from '@/library/User';
+import { type PurchasedItem, type Token } from '@/types';
+
+async function getBookFile(slug: string): Promise<Buffer> {
+	try {
+		const filePath = path.join(process.cwd(), 'src/downloads', `${slug}.pdf`);
+		await fs.access(filePath);
+		const fileBuffer = await fs.readFile(filePath);
+		return fileBuffer;
+	} catch (error) {
+		console.error(`Error reading book file: ${error}`);
+		throw new Error(`Book file not found or inaccessible: ${slug}`);
+	}
+}
+
+export async function GET(
+	res: NextApiResponse,
+	context: { params: { slug: string } }
+) {
+	try {
+		await connectToDatabase();
+
+		const cookieStore = cookies();
+		const token = cookieStore.get('token');
+
+		if (!token) {
+			return NextResponse.json({ error: 'No token found' }, { status: 401 });
+		}
+
+		try {
+			const decodedToken = jwt.verify(
+				token.value,
+				process.env.JWT_SECRET!
+			) as Token;
+
+			const user = await User.findById(decodedToken.sub);
+
+			if (!user) {
+				return NextResponse.json(
+					{ error: 'User not found' },
+					{ status: 404 }
+				);
+			}
+
+			const { slug } = context.params;
+			if (typeof slug !== 'string') {
+				return NextResponse.json(
+					{ error: 'Invalid slug' },
+					{ status: 400 }
+				);
+			}
+
+			const purchasedItem = user.purchased.find(
+				(item: PurchasedItem) => item.slug === slug
+			);
+			if (!purchasedItem || purchasedItem.downloads <= 0) {
+				return res.status(403).json({ error: 'No downloads remaining' });
+			}
+
+			const fileBuffer = await getBookFile(slug);
+
+			purchasedItem.downloads -= 1;
+			await user.save();
+
+			return new NextResponse(fileBuffer, {
+				headers: {
+					'Content-Type': 'application/pdf',
+					'Content-Disposition': `attachment; filename="${slug}.pdf"`,
+				},
+			});
+		} catch (jwtError) {
+			console.error('JWT verification error:', jwtError);
+			return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
+		}
+	} catch (error) {
+		console.error('Download error:', error);
+		return NextResponse.json(
+			{ error: 'Internal server error' },
+			{ status: 500 }
+		);
+	}
+}
