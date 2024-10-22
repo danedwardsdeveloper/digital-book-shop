7243a4c46cecf85aad1d4c6878d8c77c569ff8f1
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-22 18:14:54 +0100

    Update tests (passing)

--- test/main.test.ts
+++ test/main.test.ts
@@ -65,30 +65,44 @@ describe('Digital Book Shop', () => {
 		const currentUrl = page.url();
 		expect(currentUrl).toMatch(/^https:\/\/checkout\.stripe\.com/);
 	}, 10000);
-	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 	it('should complete Stripe checkout form and redirect to account page', async () => {
-		// Fill out the form
 		await page.type('#email', 'test@gmail.com');
 		await page.type('#cardNumber', '4242424242424242');
 		await page.type('#cardExpiry', '0130');
 		await page.type('#cardCvc', '123');
 		await page.type('#billingName', 'Test Person');
-
-		await page.select('#billingCountry', 'United Kingdom');
-
-		await page.waitForSelector('#billingPostalCode');
+		await page.select('#billingCountry', 'GB');
+		await page.waitForSelector('#billingPostalCode', {
+			visible: true,
+			timeout: 5000,
+		});
 		await page.type('#billingPostalCode', 'SW1A 1AA');
-
 		const submitButton = await getByTestId('hosted-payment-submit-button');
 		await Promise.all([
 			submitButton.click(),
 			page.waitForNavigation({ waitUntil: 'networkidle0' }),
 		]);
-
 		const currentUrl = page.url();
-		expect(currentUrl).toBe(`${BASE_URL}account/`);
+		expect(currentUrl).toBe(`${BASE_URL}account`);
 	}, 30000);
+	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+	it(`shows the purchased book in the "Purchase history" table with 5 downloads remaining`, async () => {
+		const downloadButton = await getByTestId('dracula-download-button');
+		const downloadButtonText = await downloadButton.evaluate(
+			(element) => element.textContent
+		);
+		expect(downloadButtonText).toBe('Download');
+
+		const downloadsRemaining = await getByTestId(
+			'dracula-downloads-remaining'
+		);
+		const downloadsRemainingText = await downloadsRemaining.evaluate(
+			(element) => element.textContent
+		);
+		expect(downloadsRemainingText).toBe('5');
+	});
 
 	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
