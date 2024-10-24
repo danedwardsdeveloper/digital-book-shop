34a3e497cd9d3d55a4db5ecda3a8ed0b2fc2479e
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-24 12:07:34 +0100

    Update styles

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -13,9 +13,9 @@ export default function BookPage({ params }: { params: { slug: string } }) {
 	}
 
 	return (
-		<div className="container mx-auto px-4 py-8 space-y-8">
+		<div className="container mx-auto px-4 py-8 space-y-12">
 			<div className="flex flex-col md:flex-row md:space-x-8">
-				<div className="w-full md:w-1/2 md:mb-0 order-2 md:order-1">
+				<div className="w-full md:w-1/2 md:mb-0">
 					<div>
 						<Image
 							priority
@@ -28,23 +28,47 @@ export default function BookPage({ params }: { params: { slug: string } }) {
 				<div
 					className={clsx(
 						'w-full md:w-1/2 space-y-4',
-						'order-1 md:order-2 mb-8'
+						'mt-8 md:mt-0',
+						'flex flex-col justify-between'
 					)}
 				>
-					<div className="space-y-1">
-						<h1 className="text-3xl font-bold">{book.title}</h1>
-						<p className="text-xl text-gray-600">{book.author}</p>
+					<div className={clsx('space-y-1', 'mb-10 md:mb-0')}>
+						<h1
+							className={clsx(
+								'text-3xl font-bold',
+								'text-left md:text-right'
+							)}
+						>
+							{book.title}
+						</h1>
+						<p
+							className={clsx(
+								'text-xl text-gray-600',
+								'text-left md:text-right'
+							)}
+						>
+							{book.author}
+						</p>
 					</div>
-					<p>£{book.price.toFixed(2)}</p>
-					<p className="text-gray-700">{`This exclusive digital edition is meticulously typeset to bring ${book.author}'s prose to life for contemporary readers. Purchase now and lose yourself in this unforgettable classic!`}</p>
-					<CartButton
-						slug={book.slug}
-						variant={'button'}
-						dataTestID={`cart-button-${book.slug}`}
-					/>
+					<div className="space-y-4">
+						<p
+							className={clsx(
+								'font-semibold text-lg',
+								'text-left md:text-right'
+							)}
+						>
+							£{book.price.toFixed(2)}
+						</p>
+						<p className="text-gray-700 text-justify">{`This exclusive digital edition is meticulously typeset to bring ${book.author}'s prose to life for contemporary readers. Purchase now and lose yourself in this unforgettable classic!`}</p>
+						<CartButton
+							slug={book.slug}
+							variant={'button'}
+							dataTestID={`cart-button-${book.slug}`}
+						/>
+					</div>
 				</div>
 			</div>
-			<div className="space-y-4">
+			<div className="space-y-6">
 				{book.description.map((paragraph, index) => (
 					<p key={index} className="text-gray-700">
 						{paragraph}
