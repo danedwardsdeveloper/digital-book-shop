4fac73910f4dedf67cc36e19ec30950400e0b991
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-27 09:15:08 +0000

    Add new tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,141 +1,196 @@
 import { describe, it, expect, beforeAll, afterAll } from 'vitest';
-import { page, closeBrowser } from './setup.test';
+
+import { page, setupTest, closeBrowser, baseURL } from './setup.test';
 import {
 	verifySignedInNavbar,
 	verifySignedOutNavbar,
 	verifyFeedbackMessage,
-	getByTestId,
 	clickBookLink,
-	verifyAddToCart,
+	createAccount,
+	continueShopping,
+	addToCart,
+	findInLocalStorageCart,
+	expectCartCountToBe,
+	verifyButton,
+	removeFromCart,
+	clickNavLink,
+	waitForNavigationComplete,
+	clickElement,
+	expectUrlToBe,
 } from './utilities.test';
-import { setupTest, BASE_URL } from './setup.test';
+
+beforeAll(async () => {
+	await setupTest();
+});
+
+afterAll(async () => {
+	await closeBrowser();
+});
 
 describe('Digital Book Shop', () => {
-	beforeAll(async () => {
-		await setupTest();
-	});
-
-	afterAll(async () => {
-		await closeBrowser();
-	});
-
-	it('should display correct navbar items when signed out', async () => {
-		await page.goto(BASE_URL);
-		await verifySignedOutNavbar();
-	});
-
-	it('should create an account', async () => {
-		await page.goto(`${BASE_URL}/create-account`, {
-			waitUntil: 'networkidle0',
+	describe('Signed-out functionality', () => {
+		it('should display correct navbar items when signed out', async () => {
+			await page.goto(baseURL, {
+				waitUntil: 'networkidle0',
+			});
+			await verifySignedOutNavbar();
 		});
-		await page.waitForSelector('[data-testid="name-input"]');
-		await page.type('[data-testid="name-input"]', 'Test');
-		await page.type('[data-testid="email-input"]', 'test@gmail.com');
-		await page.type('[data-testid="password-input"]', 'securePassword');
-		await Promise.all([
-			page.click('[data-testid="create-account-button"]'),
-			page.waitForNavigation({ waitUntil: 'networkidle0' }),
-		]);
-		expect(page.url()).toBe(BASE_URL);
-		await verifyFeedbackMessage(
-			'Welcome Test! Your account has been created.'
-		);
-	});
 
-	it('should display signed-in navbar items after creating an account', async () => {
-		await page.goto(BASE_URL, {
-			waitUntil: 'networkidle0',
+		describe('Add books to cart', () => {
+			it('should add Dracula', async () => {
+				await clickBookLink('dracula');
+				await verifyButton('cart-button-dracula', 'Add to cart');
+				await addToCart('dracula');
+				await expectCartCountToBe(1);
+				await findInLocalStorageCart(`"dracula":false`);
+				await continueShopping();
+			});
+
+			it('should add Jane Eyre', async () => {
+				await clickBookLink('jane-eyre');
+				await verifyButton('cart-button-jane-eyre', 'Add to cart');
+				await addToCart('jane-eyre');
+				await expectCartCountToBe(2);
+				await findInLocalStorageCart(`"jane-eyre":false`);
+				await continueShopping();
+			});
+
+			it('should add Wuthering Heights', async () => {
+				await clickBookLink('wuthering-heights');
+				await verifyButton('cart-button-wuthering-heights', 'Add to cart');
+				await addToCart('wuthering-heights');
+				await expectCartCountToBe(3);
+				await findInLocalStorageCart(`"wuthering-heights":false`);
+				await continueShopping();
+			});
 		});
-		await verifySignedInNavbar();
-	});
 
-	it('should add a book to the cart', async () => {
-		await clickBookLink('dracula');
-		await verifyAddToCart('dracula', 1);
-	});
-
-	it('should redirect to "checkout.stripe.com" after clicking the "Checkout" button', async () => {
-		const checkoutButton = await getByTestId('checkout-button');
-
-		await Promise.all([
-			checkoutButton.click(),
-			page.waitForNavigation({ waitUntil: 'networkidle0' }),
-		]);
-
-		const currentUrl = page.url();
-		expect(currentUrl).toMatch(/^https:\/\/checkout\.stripe\.com/);
-	}, 10000);
-
-	it('should complete Stripe checkout form and redirect to account page', async () => {
-		await page.type('#email', 'test@gmail.com');
-		await page.type('#cardNumber', '4242424242424242');
-		await page.type('#cardExpiry', '0130');
-		await page.type('#cardCvc', '123');
-		await page.type('#billingName', 'Test Person');
-		await page.select('#billingCountry', 'GB');
-		await page.waitForSelector('#billingPostalCode', {
-			visible: true,
-			timeout: 5000,
+		describe('Remove books from cart', () => {
+			it('should remove Wuthering Heights', async () => {
+				await clickBookLink('wuthering-heights');
+				await verifyButton(
+					'cart-button-wuthering-heights',
+					'Remove from cart'
+				);
+				await removeFromCart('wuthering-heights');
+				await expectCartCountToBe(2);
+				await findInLocalStorageCart(`"wuthering-heights":true`);
+				await clickNavLink('nav-home', '/');
+			});
 		});
-		await page.type('#billingPostalCode', 'SW1A 1AA');
-		const submitButton = await getByTestId('hosted-payment-submit-button');
-		await Promise.all([
-			submitButton.click(),
-			page.waitForNavigation({ waitUntil: 'networkidle0' }),
-		]);
-		const currentUrl = page.url();
-		expect(currentUrl).toBe(`${BASE_URL}account`);
-	}, 30000);
-	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-	it(`shows the purchased book in the "Purchase history" table with 5 downloads remaining`, async () => {
-		const downloadButton = await getByTestId('dracula-download-button');
-		const downloadButtonText = await downloadButton.evaluate(
-			(element) => element.textContent
-		);
-		expect(downloadButtonText).toBe('Download');
-
-		const downloadsRemaining = await getByTestId(
-			'dracula-downloads-remaining'
-		);
-		const downloadsRemainingText = await downloadsRemaining.evaluate(
-			(element) => element.textContent
-		);
-		expect(downloadsRemainingText).toBe('5');
 	});
 
-	/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-	it('should delete the account', async () => {
-		await page.goto(`${BASE_URL}/account`, { waitUntil: 'networkidle0' });
-		await page.waitForSelector('[data-testid="delete-account-button"]', {
-			visible: true,
-			timeout: 5000,
-		});
-		const deleteButton = await page.$(
-			'[data-testid="delete-account-button"]'
-		);
-		expect(deleteButton).not.toBeNull();
-		page.on('dialog', async (dialog) => {
-			expect(dialog.message()).toContain(
-				'Are you sure you want to delete your account?'
+	describe('Create account', () => {
+		it('should create an account', async () => {
+			await createAccount();
+			expect(page.url()).toBe(baseURL);
+			await verifyFeedbackMessage(
+				'Welcome Test! Your account has been created.'
 			);
-			await dialog.accept();
 		});
-		await deleteButton?.click();
-		await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+		it('should still have 2 items in the cart', async () => {
+			await expectCartCountToBe(2);
+		});
+
+		it('should display signed-in menu items', async () => {
+			await page.goto(baseURL, {
+				waitUntil: 'networkidle0',
+			});
+			await verifySignedInNavbar();
+		});
+
+		it('should add Wuthering Heights', async () => {
+			await clickBookLink('wuthering-heights');
+			await verifyButton('cart-button-wuthering-heights', 'Add to cart');
+			await addToCart('wuthering-heights');
+			await expectCartCountToBe(3);
+			await findInLocalStorageCart(`"wuthering-heights":false`);
+			await continueShopping();
+		});
 	});
 
-	it(`should display an 'account deleted' message`, async () => {
-		expect(page.url()).toBe(BASE_URL);
-		await verifyFeedbackMessage('Account deleted successfully');
+	describe('Sign out', () => {
+		it('should sign out', async () => {
+			await clickElement('nav-account');
+			await clickElement('sign-out-button');
+			await expectUrlToBe('/');
+		});
+
+		it(`should have deleted the "token" cookie and "cart" local storage`, async () => {
+			const cookies = await page.cookies();
+			const tokenCookie = cookies.find((cookie) => cookie.name === 'token');
+			expect(tokenCookie).toBeUndefined();
+			const cartItem = await page.evaluate(() =>
+				localStorage.getItem('cart')
+			);
+			expect(cartItem).toBeNull();
+		});
+
+		it('should have no items in the cart', async () => {
+			await expectCartCountToBe(0);
+		});
+
+		it('should add Madame Bovary', async () => {
+			await clickBookLink('madame-bovary');
+			await verifyButton('cart-button-madame-bovary', 'Add to cart');
+			await addToCart('madame-bovary');
+			await expectCartCountToBe(1);
+			await findInLocalStorageCart(`"madame-bovary":false`);
+			await continueShopping();
+		});
 	});
 
-	it(`"token" cookie and "cart" local storage should not exist after account deletion`, async () => {
-		const cookies = await page.cookies();
-		const tokenCookie = cookies.find((cookie) => cookie.name === 'token');
-		expect(tokenCookie).toBeUndefined();
-		const cartItem = await page.evaluate(() => localStorage.getItem('cart'));
-		expect(cartItem).toBeNull();
+	describe('Sign in', () => {
+		it('should sign in', async () => {
+			await clickElement('nav-sign-in');
+			await clickElement('sign-out-button');
+			await expectUrlToBe('/');
+		});
+
+		it('should display signed-in menu items', async () => {
+			await page.goto(baseURL, {
+				waitUntil: 'networkidle0',
+			});
+			await verifySignedInNavbar();
+		});
+
+		it('should have 4 items in the cart', async () => {
+			await expectCartCountToBe(4);
+		});
+	});
+
+	describe('Delete account', () => {
+		it('should delete the account', async () => {
+			await page.goto(`${baseURL}account`);
+			const dialogPromise = new Promise<void>((resolve) => {
+				page.once('dialog', async (dialog) => {
+					console.log('Dialog appeared:', dialog.message());
+					expect(dialog.message()).toContain(
+						'Are you sure you want to delete your account?\nAccess to purchased books will be lost.'
+					);
+					await dialog.accept();
+					console.log('Dialog accepted');
+					resolve();
+				});
+			});
+			await clickElement('delete-account-button');
+			await Promise.all([dialogPromise, waitForNavigationComplete()]);
+		});
+
+		it(`should display an 'account deleted' message`, async () => {
+			expect(page.url()).toBe(baseURL);
+			await verifyFeedbackMessage('Account deleted successfully');
+		});
+
+		it(`should have deleted the "token" cookie and "cart" local storage`, async () => {
+			const cookies = await page.cookies();
+			const tokenCookie = cookies.find((cookie) => cookie.name === 'token');
+			expect(tokenCookie).toBeUndefined();
+			const cartItem = await page.evaluate(() =>
+				localStorage.getItem('cart')
+			);
+			expect(cartItem).toBeNull();
+		});
 	});
 });
