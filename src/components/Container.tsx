e055f4fc888c6490773885287bcc19eaf6872b16
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-28 09:55:34 +0000

    Update styles, create Spacer

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,20 @@
 import { ReactNode } from 'react';
+import clsx from 'clsx';
 
-export default function Container({ children }: { children: ReactNode }) {
+export function Container({ children }: { children: ReactNode }) {
 	return (
-		<div className="w-2/3 mx-auto flex flex-col mt-8 space-y-4">
+		<div
+			className={clsx(
+				'container',
+				'w-full',
+				'md:w-2/3 mx-auto flex flex-col space-y-4'
+			)}
+		>
 			{children}
 		</div>
 	);
+}
+
+export function Spacer() {
+	return <div className="h-8" />;
 }
