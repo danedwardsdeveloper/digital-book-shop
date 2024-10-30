fb5b2cbb97656c751c9f37a92f1b41a7eb904a26
Dan Edwards <daniel.edwards96@yahoo.com>
2024-10-30 10:06:40 +0000

    Add styling and Account button

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { useEffect } from 'react';
 import { useCart } from '@/providers/CartProvider';
+import { ContainerWithHeading, Spacer } from '@/components/Container';
+import { NavButton } from '@/components/Buttons';
 
 export default function ThankYou() {
 	const { clearCart } = useCart();
@@ -9,5 +11,14 @@ export default function ThankYou() {
 		clearCart();
 	}, []);
 
-	return <div>Success. Thank you for your purchase.</div>;
+	return (
+		<ContainerWithHeading heading="Thank you">
+			<p>Purchase successful.</p>
+			<Spacer />
+			<NavButton
+				text={'View purchases in account'}
+				href={'/account'}
+			></NavButton>
+		</ContainerWithHeading>
+	);
 }
